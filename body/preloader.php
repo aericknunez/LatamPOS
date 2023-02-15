db07fe8acb0c3f654798d309a0e927c48e143140
aericknunez <aerick.nunez@gmail.com>
2023-02-14 19:35:40 -0600

    Update preloader.php

--- body/preloader.php
+++ body/preloader.php
@@ -4,20 +4,29 @@
     <div class="animation-preloader">
         <div class="spinner"></div>
         <div class="txt-loading">
+            <span data-text-preloader="L" class="letters-loading">
+                L
+            </span>
+            <span data-text-preloader="a" class="letters-loading">
+                a
+            </span>
+            <span data-text-preloader="t" class="letters-loading">
+                t
+            </span>
+            <span data-text-preloader="a" class="letters-loading">
+                a
+            </span>
+            <span data-text-preloader="m" class="letters-loading">
+                m
+            </span>
             <span data-text-preloader="P" class="letters-loading">
                 P
             </span>
-            <span data-text-preloader="I" class="letters-loading">
-                I
-            </span>
-            <span data-text-preloader="Z" class="letters-loading">
-                Z
-            </span>
-            <span data-text-preloader="T" class="letters-loading">
-                T
-            </span>
             <span data-text-preloader="O" class="letters-loading">
                 O
+            </span>
+            <span data-text-preloader="S" class="letters-loading">
+                S
             </span>
 <!--             <span data-text-preloader="A" class="letters-loading">
                 A
